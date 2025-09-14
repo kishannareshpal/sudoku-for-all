e178c14227129f4d98c2bec024236161b9cf8b31
Kishan Jadav <kishan_jadav@hotmail.com>
2025-09-14 22:52:02 +0100

    Basic native control setup via expo modules

--- modules/sudoku-for-all-ui/index.ts
+++ modules/sudoku-for-all-ui/index.ts
@@ -1,5 +1,5 @@
 // Reexport the native module. On web, it will be resolved to SudokuForAllUIModule.web.ts
 // and on native platforms to SudokuForAllUIModule.ts
-export { default } from './src/SudokuForAllUIModule';
-export { default as SudokuForAllUIView } from './src/SudokuForAllUIView';
-export * from  './src/SudokuForAllUI.types';
+
+import SudokuForAllUIView from "./src/SudokuForAllUIView";
+export { SudokuForAllUIView };
